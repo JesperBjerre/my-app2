4ab548eaefcfd7f0e5f0625818c1472f2ac89b3a
Jesper Hyldenbrandt Bjerre <jhb@unik.dk>
2022-04-27 13:32:33 +0200

    Azure Static Web App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cloudflare demo
+          Azure Static Web App
         </p>
         <a
           className="App-link"
